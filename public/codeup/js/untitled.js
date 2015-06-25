11bfd1a482b5b7e25a586a271cae8a239b765cc9
Johnathan Christopherson <johnathan.christopherson@live.com>
2015-06-25 10:05:25 -0500

    Calc and Map

---
+++ public/codeup/js/untitled.js
@@ -0,0 +1,102 @@
+document.getElementsByTagName('button')[0].addEventListener('click', function(){
+
+ if (document.calculator.number2.value == '') { 
+  number += '1'; document.calculator.number1.value =(number);
+ } else {
+  newnumber += '1'; document.calculator.total.value = (newnumber)
+ }
+
+}, false);
+
+document.getElementsByTagName('button')[1].addEventListener('click', function(){
+
+ if (document.calculator.number2.value == '') { 
+  number += '2'; document.calculator.number1.value =(number);
+ } else {
+  newnumber += '2'; document.calculator.total.value = (newnumber)
+ }
+
+}, false);
+
+document.getElementsByTagName('button')[2].addEventListener('click', function(){
+
+ if (document.calculator.number2.value == '') { 
+  number += '3'; document.calculator.number1.value =(number);
+ } else {
+  newnumber += '3'; document.calculator.total.value = (newnumber)
+ }
+
+}, false);
+
+document.getElementsByTagName('button')[3].addEventListener('click', function(){
+
+ if (document.calculator.number2.value == '') { 
+  number += '4'; document.calculator.number1.value =(number);
+ } else {
+  newnumber += '4'; document.calculator.total.value = (newnumber)
+ }
+
+}, false);
+
+document.getElementsByTagName('button')[4].addEventListener('click', function(){
+
+ if (document.calculator.number2.value == '') { 
+  number += '5'; document.calculator.number1.value =(number);
+ } else {
+  newnumber += '5'; document.calculator.total.value = (newnumber)
+ }
+
+}, false);
+
+document.getElementsByTagName('button')[5].addEventListener('click', function(){
+
+ if (document.calculator.number2.value == '') { 
+  number += '6'; document.calculator.number1.value =(number);
+ } else {
+  newnumber += '6'; document.calculator.total.value = (newnumber)
+ }
+
+}, false);
+
+document.getElementsByTagName('button')[6].addEventListener('click', function(){
+
+ if (document.calculator.number2.value == '') { 
+  number += '7'; document.calculator.number1.value =(number);
+ } else {
+  newnumber += '7'; document.calculator.total.value = (newnumber)
+ }
+
+}, false);
+
+document.getElementsByTagName('button')[7].addEventListener('click', function(){
+
+ if (document.calculator.number2.value == '') { 
+  number += '8'; document.calculator.number1.value =(number);
+ } else {
+  newnumber += '8'; document.calculator.total.value = (newnumber)
+ }
+
+}, false);
+
+document.getElementsByTagName('button')[8].addEventListener('click', function(){
+
+ if (document.calculator.number2.value == '') { 
+  number += '9'; document.calculator.number1.value =(number);
+ } else {
+  newnumber += '9'; document.calculator.total.value = (newnumber)
+ }
+
+}, false);
+
+document.getElementsByTagName('button')[9].addEventListener('click', function(){
+
+ if (document.calculator.number2.value == '') { 
+  number += '0'; document.calculator.number1.value =(number);
+  console.log (number);
+ } else {
+  newnumber += '0'; document.calculator.total.value = (newnumber)
+  console.log (newnumber);
+
+ }
+
+}, false);
