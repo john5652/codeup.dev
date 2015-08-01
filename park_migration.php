5d487ced894ef6852e6fd8e1d41578d5ddaf63b6
Johnathan Christopherson <johnathan.christopherson@live.com>
2015-07-31 21:00:54 -0500

    Park_Seeder

---
+++ park_migration.php
@@ -0,0 +1,20 @@
+<?php
+define('DB_HOST', '127.0.0.1');
+define('DB_NAME', 'parks_db');
+define('DB_USER', 'billy');
+define('DB_PASS', '');
+
+require 'db_connect.php';
+
+$dbc->exec('DROP TABLE IF EXISTS `national_parks`');
+
+$dbc->exec(
+ 'CREATE TABLE national_parks (
+    id INT UNSIGNED NOT NULL AUTO_INCREMENT,
+    name VARCHAR(50) NOT NULL,
+    location VARCHAR(50) NOT NULL,
+    date_established DATE NOT NULL,
+    area_in_acres FLOAT(12,2) NOT NULL,
+    PRIMARY KEY (id)
+  )'
+);
